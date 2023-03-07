a076a128d17f500ab7eef7d1a3543b113a82f85b
Yoshi252 <yurithewriter@gmail.com>
2023-03-07 06:37:10 -0800

    modified:   App.js 	modified:   app.json 	new file:   assets/SplashScreen.png 	new file:   assets/fonts/Roboto-Black.ttf 	new file:   assets/fonts/Roboto-BlackItalic.ttf 	new file:   assets/fonts/Roboto-Bold.ttf 	new file:   assets/fonts/Roboto-BoldItalic.ttf 	new file:   assets/fonts/Roboto-Italic.ttf 	new file:   assets/fonts/Roboto-Light.ttf 	new file:   assets/fonts/Roboto-LightItalic.ttf 	new file:   assets/fonts/Roboto-Medium.ttf 	new file:   assets/fonts/Roboto-MediumItalic.ttf 	new file:   assets/fonts/Roboto-Regular.ttf 	new file:   assets/fonts/Roboto-Thin.ttf 	new file:   assets/fonts/Roboto-ThinItalic.ttf 	deleted:    assets/splash.png 	modified:   package.json 	modified:   yarn.lock

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font'
+
+// This prevents the splash screen from going away
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // This is the best way to use async with useEffect 
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          "black": require("./assets/fonts//Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        }); 
+      } 
+      catch (error) {
+        console.log(error)
+      }
+      finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded])
+
+  // We must add this for it to work
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider 
+      style={styles.container}
+      onLayout={onLayout}
+    >
+      <SafeAreaView>
+ 
+        <Text style={styles.label}>Hey Yuri!</Text>
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +74,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: "black"
+  }
 });
